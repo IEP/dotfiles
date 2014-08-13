49ebaf885fb8f5d42b9c7f31690bb8d7915b2481
Ivan Ega Pratama <ivan.ega.p@gmail.com>
2014-08-13 19:21:02 +0700

    edit tanggalan

--- .xmonad/script/tanggalan.sh
+++ .xmonad/script/tanggalan.sh
@@ -4,5 +4,5 @@ fi
 
 calen=`cal | sed -e "s/$(date | cut -d' ' -f3)/^fg(#FF8000)$(date | cut -d' ' -f3)^fg()/"`
 touch /tmp/dzen-cal
-(echo "$calen"; sleep 5) | dzen2 -x 1180 -y 30 -l 8 -w 170 -h 16 -e 'onstart=uncollapse;button1=exit,exec:rm /tmp/dzen-cal;button3=exit,exec:rm /tmp/dzen-cal' -fn "Meslo LG S for Powerline:size=10" -sa c -bg "#3F3F3F" -fg "#B3B3B3"
+(echo "$calen"; sleep 5) | dzen2 -x 1180 -y 30 -l 8 -w 170 -h 16 -e 'onstart=uncollapse;button1=exit,exec:rm /tmp/dzen-cal;button3=exit,exec:rm /tmp/dzen-cal' -fn "Meslo LG S for Powerline:size=10" -sa c -bg "#4F4F4F" -fg "#B3B3B3"
 rm /tmp/dzen-cal
