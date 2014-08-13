8ecb746758a485ddcc97f1671552306e4edfff24
Ivan Ega Pratama <ivan.ega.p@gmail.com>
2014-08-13 11:06:44 +0700

    edit tanggalan.sh for preventing multiple instances

--- .xmonad/script/tanggalan.sh
+++ .xmonad/script/tanggalan.sh
@@ -1,3 +1,8 @@
-calen=`cal | sed -e "s/$(date | cut -d' ' -f3)/^fg(#FF8000)$(date | cut -d' ' -f3)^fg()/"`
+if [ -f /tmp/dzen-cal ]; then
+        exit
+fi
 
-(echo "$calen"; sleep 5) | dzen2 -x 1180 -y 30 -l 8 -w 170 -h 16 -e 'onstart=uncollapse;button1=exit;button3=exit' -fn "Meslo LG S for Powerline:size=10" -sa c -bg "#3F3F3F" -fg "#B3B3B3"
+calen=`cal | sed -e "s/$(date | cut -d' ' -f3)/^fg(#FF8000)$(date | cut -d' ' -f3)^fg()/"`
+touch /tmp/dzen-cal
+(echo "$calen"; sleep 5) | dzen2 -x 1180 -y 30 -l 8 -w 170 -h 16 -e 'onstart=uncollapse;button1=exit,exec:rm /tmp/dzen-cal;button3=exit,exec:rm /tmp/dzen-cal' -fn "Meslo LG S for Powerline:size=10" -sa c -bg "#3F3F3F" -fg "#B3B3B3"
+rm /tmp/dzen-cal
