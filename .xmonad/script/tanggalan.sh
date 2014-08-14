f7d6586f797709089da63a1c1a017df11100a54f
Ivan Ega Pratama <ivan.ega.p@gmail.com>
2014-08-14 09:35:18 +0700

    fixing syntax in tanggalan

--- .xmonad/script/tanggalan.sh
+++ .xmonad/script/tanggalan.sh
@@ -2,7 +2,7 @@ if [ -f /tmp/dzen-cal ]; then
         exit
 fi
 
-calen=`cal | sed -e "s/$(date | cut -d' ' -f3)/^fg(#FF8000)$(date | cut -d' ' -f3)^fg()/"`
+calen=`cal | sed -e "s/ $(date | cut -d' ' -f3) / ^fg(#FF8000)$(date | cut -d' ' -f3)^fg() /"`
 touch /tmp/dzen-cal
 (echo "$calen"; sleep 5) | dzen2 -x 1180 -y 30 -l 8 -w 170 -h 16 -e 'onstart=uncollapse;button1=exit,exec:rm /tmp/dzen-cal;button3=exit,exec:rm /tmp/dzen-cal' -fn "Meslo LG S for Powerline:size=10" -sa c -bg "#4F4F4F" -fg "#B3B3B3"
 rm /tmp/dzen-cal
